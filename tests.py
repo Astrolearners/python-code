62c88f2e5e3192a776954075bbfa9e28722433d8
Stavros <steveiliop56@gmail.com>
2024-01-27 15:46:23 +0200

    refactor(tests): disable tests for now

--- tests.py
+++ tests.py
@@ -1,28 +1,24 @@
 from .modules.sensors import bme680
 from .modules.communication import gps
 from .modules.communication import radio
-<<<<<<< HEAD
 from .modules.helpers import csv_handler
-=======
-from .modules.helpers import csv_handler, nmea_parser
->>>>>>> 4559cd12a56fd86b54b8e0728a761398ea239eed
 from .modules.other import buzzer
 
-print("INFO: Running tests...")
+# print("INFO: Running tests...")
 
-print("TESTS: Communication tests...")
+# print("TESTS: Communication tests...")
 
-gps.test()
-radio.test()
+# gps.test()
+# radio.test()
 
-print("TESTS: Running sensor tests...")
+# print("TESTS: Running sensor tests...")
 
-bme680.test()
+# bme680.test()
 
-print("TESTS: Running other tests...")
+# print("TESTS: Running other tests...")
 
-buzzer.test()
+# buzzer.test()
 
-print("TESTS: Running helper tests...")
+# print("TESTS: Running helper tests...")
 
-nmea_parser.test()
+# nmea_parser.test()
