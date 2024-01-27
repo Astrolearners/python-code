99df5ace13f429e3d0ce3aac7c19eccf8ad4b78a
Stavros <steveiliop56@gmail.com>
2024-01-27 17:28:05 +0200

    feat(modules): add buzzer

--- modules/other/buzzer.py
+++ modules/other/buzzer.py
@@ -1,11 +1,30 @@
-def buzz():
-    print("Buzzing buzzer...")
-    return "OK"
+from gpiozero import Buzzer
+from time import sleep
 
-def test():
-    print("TESTS: Bipping buzzer.")
-    try:
-        print(buzz())
-        print("TESTS: Buzzer OK.")
-    except:
-        print("TESTS: Buzzer ERROR.")
+from modules.helpers.logger import logger
+
+class buzzer():
+    def __init__(self):
+        self.log = logger("buzzer")
+
+        self.log.info("Initializing buzzer...")
+        try:
+            self.buzzer = Buzzer(40)
+        except Exception as e:
+            self.log.error(f"Failed to initialize buzzer! Error: {e}")
+
+    def beep(self, delay, times):
+        self.log.debug(f"Turning buzzer on and sleeping {delay} second(s) for {times} time(s)...")
+        for i in range(times):
+            self.buzzer.on()
+            sleep(delay)
+            self.buzzer.off()
+
+# Tests not ready
+# def test():
+#     print("TESTS: Bipping buzzer.")
+#     try:
+#         print(buzz())
+#         print("TESTS: Buzzer OK.")
+#     except:
+#         print("TESTS: Buzzer ERROR.")
