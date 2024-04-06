87bf9b733dcb10fc32f810c3ea7c9af198f0e488
Stavros <steveiliop56@gmail.com>
2024-04-06 17:08:08 +0300

    refactor(csv_handler): use if else instead of match

--- cansat/modules/helpers/csv_handler.py
+++ cansat/modules/helpers/csv_handler.py
@@ -21,30 +21,24 @@ class csvHandler():
     def addData(self, data: dict):
         newData = self.dataframe_schema
         for i in data.keys():
-            match i:
-                case "time":
-                    newData["time"] = data[i]
-                case "bme":
-                    for b in self.main_schema["bme"].keys():
-                        newData[b] = data["bme"][b]
-                case "gps":
-                    for g in main_schema["gps"].keys():
-                        newData[g] = data["gps"][g]
-                case "accelerometer":
-                    for a in main_schema["accelerometer"].keys():
-                        match a:
-                            case "accelerometer":
-                                for a_a in self.main_schema["accelerometer"]["accelerometer"].keys():
-                                    newData[f"accelerometer-{a_a}"] = data["accelerometer"]["accelerometer"][a_a]
-                            case "magnetometer":
-                                for a_m in self.main_schema["accelerometer"]["magnetometer"].keys():
-                                    newData[f"magnetometer-{a_m}"] = data["accelerometer"]["magnetometer"][a_m]
-                            case _:
-                                pass
-                case "rpi_temp":
-                    newData["rpi_temp"] = data[i]
-                case _:
-                    pass
+            if i == "time":
+                newData["time"] = data[i]
+            elif i == "bme":
+                for b in self.main_schema["bme"].keys():
+                    newData[b] = data["bme"][b]
+            elif i == "gps":
+                for g in main_schema["gps"].keys():
+                    newData[g] = data["gps"][g]
+            elif i == "accelerometer":
+                for a in main_schema["accelerometer"].keys():
+                    if a == "accelerometer":
+                        for a_a in self.main_schema["accelerometer"]["accelerometer"].keys():
+                            newData[f"accelerometer-{a_a}"] = data["accelerometer"]["accelerometer"][a_a]
+                    if a == "magnetometer":
+                        for a_m in self.main_schema["accelerometer"]["magnetometer"].keys():
+                            newData[f"magnetometer-{a_m}"] = data["accelerometer"]["magnetometer"][a_m]
+            elif i == "rpi_temp":
+                newData["rpi_temp"] = data[i]
         self.dataFrame.loc[len(self.dataFrame.index)] = newData
 
     def saveDataframe(self):
